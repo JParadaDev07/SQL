001552c213dfe8eece0176d060c13d335e505895
Julián Parada <alexgualteros02@gmail.com>
2026-03-11 13:08:16 -0500

    Refine SQL query to filter Data Analyst jobs by specific locations and remote work options

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -21,8 +21,11 @@ FROM
 LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE
     job_title_short = 'Data Analyst' AND
-    job_location IN ('Anywhere', 'Colombia') AND
-    salary_year_avg IS NOT NULL
+    salary_year_avg IS NOT NULL AND
+    AND (
+            job_work_from_home = TRUE
+            OR job_location = ('Bogotá, Bogota, Colombia')
+        )
 ORDER BY
     salary_year_avg DESC
 LIMIT 50
