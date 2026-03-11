1d30c730bcb11c22d4a03cf3c23d593e2352a1c9
Julián Parada <alexgualteros02@gmail.com>
2026-03-11 13:33:08 -0500

    Fix SQL query to correctly reference job title column for Data Analyst

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -1,6 +1,6 @@
 SELECT
     job_id,
-    job_title_short,
+    job_title_short job_title,
     job_location,
     job_schedule_type,
     salary_year_avg,
@@ -10,7 +10,7 @@ FROM
     job_postings_fact
 LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE
-    job_title_short = 'Data Analyst' AND
+    job_title = 'Data Analyst' AND
     salary_year_avg IS NOT NULL
     AND (
             job_work_from_home = TRUE
