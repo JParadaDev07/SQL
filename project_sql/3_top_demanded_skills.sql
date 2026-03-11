8a7229ccc5cb5cf734395fca81456b9fa6100cd9
Julián Parada <alexgualteros02@gmail.com>
2026-03-11 13:08:22 -0500

    Refine SQL query to filter Data Analyst jobs by remote work options and specific location

--- project_sql/3_top_demanded_skills.sql
+++ project_sql/3_top_demanded_skills.sql
@@ -5,8 +5,11 @@ FROM job_postings_fact
 INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
-    job_title_short = 'Data Analyst' AND
-    job_location IN ('Anywhere', 'Colombia')
+    job_title_short = 'Data Analyst' 
+    AND (
+            job_work_from_home = TRUE
+            OR job_location = ('Bogotá, Bogota, Colombia')
+        )
 GROUP BY
     skills
 ORDER BY
