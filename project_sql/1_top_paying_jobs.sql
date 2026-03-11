b90c1d3e3512b725935c23532e5012f644573722
Julián Parada <alexgualteros02@gmail.com>
2026-03-11 13:32:58 -0500

    Refactor SQL query to improve readability and update job title reference

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -1,16 +1,6 @@
-/*
-    Question:
-    What are the top-paying Data Analyst jobs?
-    - Identify the top 10 highest-paying Data Analyst roles that are available remotely.
-    - Focuses on the job postings with spicified salaries (remove nulls)
-    - Highlight the top-paying opportunities for Data Analysis.
-*/
-
-
-
 SELECT
     job_id,
-    job_title,
+    job_title_short,
     job_location,
     job_schedule_type,
     salary_year_avg,
@@ -21,7 +11,7 @@ FROM
 LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE
     job_title_short = 'Data Analyst' AND
-    salary_year_avg IS NOT NULL AND
+    salary_year_avg IS NOT NULL
     AND (
             job_work_from_home = TRUE
             OR job_location = ('Bogotá, Bogota, Colombia')
